22291d69a68253c471ff3307c5baf7a4f072c0be
Jonas <jonasbrinkhoff@gmail.com>
2025-01-18 14:55:46 -0600

    Split AusstattungsAbzug into it's own file

--- src/calculation/preisspanne.ts
+++ src/calculation/preisspanne.ts
@@ -1,73 +1,14 @@
 import {
-  getAusstattung,
   getBaujahrSpanne,
   getMietspiegeljahr,
   getWohnflaecheSpanne,
   getWohnlage,
 } from "~/form/api";
 import { FinalAnswers } from "~/form/flow-machine";
-import { ausstattungsAbzuegeByYear } from "~/mietspiegel/ausstattungsAbzuege";
 import { preisspannenByMietspiegeljahr } from "~/mietspiegel/preisspannen";
 import { Preisspanne } from "~/mietspiegel/types";
 
-/**
- * Calculates the worst and best possible Ausstattungsabzug ('facility discount') based on the provided answers.
- */
-export function getWorstBestAusstattungsAbzug(
-  answers: FinalAnswers,
-  visibleQuestionAliases: Set<string>,
-): { worst: number; best: number } | undefined {
-  const mietspiegelJahr = getMietspiegeljahr(answers, visibleQuestionAliases);
-  const baujahrSpanne = getBaujahrSpanne(answers, visibleQuestionAliases);
-  const ausstattung = getAusstattung(answers, visibleQuestionAliases);
-
-  if (
-    !mietspiegelJahr ||
-    !baujahrSpanne ||
-    !ausstattung.sammelheizung ||
-    !ausstattung.bad
-  ) {
-    return undefined;
-  }
-
-  const ausstattungsAbzuege =
-    ausstattungsAbzuegeByYear[
-      mietspiegelJahr as keyof typeof ausstattungsAbzuegeByYear
-    ][
-      baujahrSpanne as keyof (typeof ausstattungsAbzuegeByYear)[typeof mietspiegelJahr]
-    ];
-
-  if (!ausstattungsAbzuege) {
-    return {
-      best: 0,
-      worst: 0,
-    };
-  }
-
-  const isChecked = (m: "Ja" | "Nein" | "Nicht sicher") => m == "Nein";
-  const isMaybeOrChecked = (m: "Ja" | "Nein" | "Nicht sicher") =>
-    m == "Nein" || m == "Nicht sicher";
-
-  const best =
-    isMaybeOrChecked(ausstattung.sammelheizung) &&
-    isMaybeOrChecked(ausstattung.bad)
-      ? ausstattungsAbzuege["!SH && !Bad"]
-      : isMaybeOrChecked(ausstattung.sammelheizung) ||
-          isMaybeOrChecked(ausstattung.bad)
-        ? ausstattungsAbzuege["!SH || !Bad"]
-        : 0;
-  const worst =
-    isChecked(ausstattung.sammelheizung) && isChecked(ausstattung.bad)
-      ? ausstattungsAbzuege["!SH && !Bad"]
-      : isChecked(ausstattung.sammelheizung) || isChecked(ausstattung.bad)
-        ? ausstattungsAbzuege["!SH || !Bad"]
-        : 0;
-
-  return {
-    best: best,
-    worst: worst,
-  };
-}
+import { getWorstBestAusstattungsAbzug } from "./ausstattungsAbzug";
 
 /**
  *  Calculates the worst and best applicable Preisspanne ('rent bracket') based on the provided answers.
