25dab8c5edfe2e80e5836674de109e3ae668e6d3
Jonas <jonasbrinkhoff@gmail.com>
2025-12-23 14:38:31 +0100

    Update

--- src/pages/schnelltest/exit/exits/mieterhoehung/ExitMieterhoehungZugestimmt.tsx
+++ src/pages/schnelltest/exit/exits/mieterhoehung/ExitMieterhoehungZugestimmt.tsx
@@ -11,20 +11,20 @@ export function ExitMieterhoehungZugestimmt() {
       </h2>
       <div className="space-y-3 text-neutral-faded mb-6">
         <p>
-          In diesem Falle ist es sehr schwer die Mieterhöhung wieder
-          zurückzunehmen, selbst wenn sie nicht rechtmäßig war und du jetzt eine
-          überhöhte Miete zahlst.
+          Es ist sehr schwer eine Mieterhöhung rückgängig zu machen, der du ihr
+          bereits zugestimmt hast. Das gilt auch, wenn die Erhöhung nicht
+          rechtmäßig war und du jetzt eine zu hohe Miete zahlst.
         </p>
       </div>
       <div className="space-y-2 mb-6 text-neutral-faded">
         <h3 className="text-base-medium text-neutral">Was nun?</h3>
         <p>
-          Unter Umständen kann es sich aber trotzdem lohnen, die Mieterhöhung
-          bei einer kostenlosen Mieterberatung überprüfen zu lassen. Spätestens
-          wenn du deine nächste Mieterhöhung erhältst, solltest du diese
-          überprüfen bevor du ihr zustimmst. Zudem kannst du immer noch deine
-          ursprüngliche Miethöhe überprüfen. Die Mieterhöhung bleibt dann zwar
-          wirksam, die Basis wird aber vielleicht reduziert.
+          Unter Umständen kann es sich trotzdem lohnen, die Mieterhöhung bei
+          einer kostenlosen Mieterberatung überprüfen zu lassen.
+        </p>
+        <p>
+          Wenn du die nächste Mieterhöhung bekommst, lass sie prüfen, bevor du
+          zustimmst.
         </p>
       </div>
     </>
