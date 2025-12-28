99b28d080b5bf754958c86af73304cc78ed0239c
Jonas <jonasbrinkhoff@gmail.com>
2025-12-28 10:12:37 +0100

    Update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./src/tests",
   /* Run tests in files in parallel */
