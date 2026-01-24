f1781e67cab729f8754c81a632d822944858566e
Jonas <jonasbrinkhoff@gmail.com>
2026-01-24 12:51:28 +0100

    Update

--- src/tests/utils/steps.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from "@playwright/test";
-
-export type StepParams<Data> = {
-  page: Page;
-  data: Data;
-};
-
-export type StepRunner<Data> = (ctx: StepParams<Data>) => Promise<void>;
