0232817d0e2ccb4f61e60a2074723141886b109e
Jonas <jonasbrinkhoff@gmail.com>
2026-04-04 12:59:44 +0200

    Fix runtime warnings

--- src/L10nContext.tsx
+++ src/L10nContext.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from "react";
 import useSWRImmutable from "swr/immutable";
 import { useLocalStorage } from "usehooks-ts";
 
-import DE from "../public/locales/de.json";
+import DE from "./locales/de.json";
 
 export type Locale = "de" | "en";
 
