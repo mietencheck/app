c21f63876bc242f81668de233c64d4ac4b8868e0
= <=>
2026-04-26 16:49:51 +0200

    Hide vorherige mieterhoehung question

--- src/form/hidden-questions.tsx
+++ src/form/hidden-questions.tsx
@@ -1,4 +1,10 @@
-export const HIDDEN_QUESTIONS = ["Typ", "Ost", "Wohnlage", "Baujahr"];
+export const HIDDEN_QUESTIONS = [
+  "Typ",
+  "Vorherige Mieterhöhung",
+  "Ost",
+  "Wohnlage",
+  "Baujahr",
+];
 
 export const HIDDEN_QUESTIONS_FOR_MIETERHOEHUNG = [
   "Vertragsdatum",
